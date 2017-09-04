d370cedf62d2e1dc5ad683fdbbd92a40c03aeea9
juanbella <jbellasantos1@gmail.com>
2017-09-04 09:56:10 +0200

    add documentation

--- grib_to_df.py
+++ grib_to_df.py
@@ -5,10 +5,10 @@ import numpy as np
 import pandas as pd
 import datetime
 
-def grib_to_df():
+def grib_to_df(forcasted_day=None):
     #grib = pygrib.open("/home/iic/Escritorio/TLsTxcTOsoSYmtRzKDl0e75I4HAjqDApvbc.grb")
-    #grib = pygrib.open('/media/sf_Compartida/test_20170301.grib')
-    grib = pygrib.open('/media/sf_Compartida/Historicoprueba2.grib')
+    grib = pygrib.open('/media/sf_Compartida/test_20170301.grib')
+    #grib = pygrib.open('/media/sf_Compartida/Historicoprueba2.grib')
 
     variables = []
     dates = []
@@ -18,19 +18,23 @@ def grib_to_df():
             variables.append(grb.name)
         if not grb.analDate in dates:
             dates.append(grb.analDate)
-    print(variables)
-    print(dates)
-    print(variables[0]+ str(grib[1].values.shape))
-    dicc = {}
 
+
+
+
+    dicc = {}
     for date in dates:
 
+        if forcasted_day != None:
+            time_max = date + datetime.timedelta(days=forcasted_day)
         dicc[date] = {}
         data = grib.select(analDate=date)
         for grb in data:
             values = grb.values
             lat, lon = grb.latlons()
             time = date + datetime.timedelta(hours=grb['endStep'])
+            if forcasted_day != None and time.day > time_max.day:
+                continue
             variable = grb['name']
             if not time in dicc[date]:
                 dicc[date][time] = {}
@@ -44,11 +48,10 @@ def grib_to_df():
     for k in dicc:
         file = open('dataframe_'+str(k)+'.txt', 'w')
         df = pd.DataFrame.from_dict(dicc[k],orient='index')
+        df.insert(0,'forcasted day',str(k))
         file.write(str(df))
         file.close()
 
-    #df = pd.DataFrame(dicc[0][0],dicc[0].keys(),variables)
-    #print(df)
 
 if __name__ == '__main__':
-    grib_to_df()
+    grib_to_df(0)
